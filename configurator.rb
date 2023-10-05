018d5fdb50cc4f2adf32c0c4efb8e7e69e4251e6
Evgeny <102020480+ArcaneX-x@users.noreply.github.com>
2023-10-05 20:56:04 +0600

    Create configurator.rb

---
+++ configurator.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Providers
+  module Yoomoney
+    class Configurator
+      attr_accessor :shop_id, :secret_key
+      attr_reader :auth
+
+      def initialize
+        @shop_id = Rails.application.credentials.dig(:yookassa, :shop_id)
+        @secret_key = Rails.application.credentials.dig(:yookassa, :api_key)
+        set_auth
+      end
+
+      def update_configuration
+        set_auth
+      end
+
+      private
+
+      def set_auth
+        @auth = "Basic #{Base64.strict_encode64("#{@shop_id}:#{@secret_key}")}"
+      end
+    end
+  end
+end
